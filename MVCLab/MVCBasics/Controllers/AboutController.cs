82cd756a22681821c94b1ed15ff05ba30dd6c70b
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-04-01 11:54:09 +0530

    Initial Commit

---
+++ MVCLab/MVCBasics/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: AboutController
+        public string Index()
+        {
+            return "This is about controller";
+        }
+
+    }
+}
