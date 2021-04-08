049a71cef225fc74723d5ada7dabfc3cef011150
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-04-08 09:15:35 +0530

    BasaeController

--- MVCLab/MVCBasics/Controllers/TeacherController.cs
+++ MVCLab/MVCBasics/Controllers/TeacherController.cs
@@ -1,129 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using DataLayer;
+﻿using DataLayer;
 using MVCBasics.Models;
 
 namespace MVCBasics.Controllers
 {
-    public class TeacherController : Controller
+    public class TeacherController : BaseController<TeacherModel>
     {
-        private readonly IBaseRepository<TeacherModel> _repo;
-
-        public TeacherController(IBaseRepository<TeacherModel> repo)
+        public TeacherController(IBaseRepository<TeacherModel> repo) : base(repo)
         {
-            _repo = repo;
         }
 
-        // GET: Employee
-        public async Task<IActionResult> Index()
-        {
-            return View(await _repo.GetAll());
-        }
-
-        // GET: Employee/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employeeModel = await _repo.Get(id);
-            if (employeeModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(employeeModel);
-        }
-
-        // GET: Employee/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Employee/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(TeacherModel employeeModel)
-        {
-            if (ModelState.IsValid)
-            {
-                await _repo.Insert(employeeModel);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employeeModel);
-        }
-
-        // GET: Employee/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employeeModel = await _repo.Get(id);
-            if (employeeModel == null)
-            {
-                return NotFound();
-            }
-            return View(employeeModel);
-        }
-
-        // POST: Employee/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, TeacherModel employeeModel)
-        {
-            if (id != employeeModel.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                await _repo.Update(employeeModel);
-
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employeeModel);
-        }
-
-        // GET: Employee/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employeeModel = await _repo.Get(id);
-            if (employeeModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(employeeModel);
-        }
-
-        // POST: Employee/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            await _repo.Delete(id);
-            return RedirectToAction(nameof(Index));
-        }
     }
 }
