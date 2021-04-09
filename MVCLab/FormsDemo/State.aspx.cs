8b03a2363bec6b6154c976bfe02423fa998b52e5
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-04-09 10:42:26 +0530

    ViewState

---
+++ MVCLab/FormsDemo/State.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FormsDemo
+{
+    public partial class State : System.Web.UI.Page
+    {
+        int counter
+        {
+            get
+            {
+                if (ViewState["counter"] is null)
+                {
+                    return 0;
+                }
+                return (int)ViewState["counter"];
+            }
+            set
+            {
+                ViewState["counter"] = value;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            lblCounter.Text = counter.ToString();
+            counter++;
+        }
+    }
+}
