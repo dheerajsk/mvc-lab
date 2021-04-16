ceb1d4913cf6cf85d72406858a931f0191686ce7
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-04-16 10:10:03 +0530

    Employee CRUD Api.

---
+++ MVCLab/ApiLab/Controllers/EmployeeController.cs
@@ -0,0 +1,97 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+using MVCBasics.Models;
+using System.Threading.Tasks;
+
+namespace ApiLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly IBaseRepository<EmployeeModel> _repo;
+
+        public EmployeeController(IBaseRepository<EmployeeModel> repo)
+        {
+            this._repo = repo;
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _repo.GetAll());
+        }
+
+
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var employeeModel = await _repo.Get(id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employeeModel);
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Post(EmployeeModel employeeModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repo.Insert(employeeModel);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Put(EmployeeModel employeeModel)
+        {
+            if (string.IsNullOrEmpty(employeeModel.ID))
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _repo.Update(employeeModel);
+
+                return Ok(employeeModel);
+            }
+            return BadRequest();
+        }
+
+        // GET: Employee/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeModel = await _repo.Get(id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+            await _repo.Delete(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ MVCLab/ApiLab/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiLab.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Get")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("Get2")]
+        public IActionResult Get2()
+        {
+            return Ok("test");
+        }
+
+        public IActionResult Post(int num)
+        {
+            if (num < 1)
+            {
+                return BadRequest("Number should be greater than 0");
+            }
+
+            return Ok("Here's your number = " + num.ToString());
+        }
+    }
+}
